d160d2404b9b76f774e32ab362729c7abd445e3e
TradeDanielP <tradedanielp@gmail.com>
2024-06-05 10:39:46 -0500

    Repositorios, interfaces de los servicios y primeras configuraciones para el manejo de errores y Swagger

---
+++ riwiMultimedia/src/main/java/com/riwi/riwiMultimedia/infrastructure/abstract_services/IClassService.java
@@ -0,0 +1,8 @@
+package com.riwi.riwiMultimedia.infrastructure.abstract_services;
+
+import com.riwi.riwiMultimedia.api.dto.request.ClassRequest;
+import com.riwi.riwiMultimedia.api.dto.response.ClassResponse;
+
+public interface IClassService extends CrudService<ClassRequest, ClassResponse, Integer> {
+    
+}
