e43f226387ffd4a04687ed58d0a7febbbbdfc570
TradeDanielP <tradedanielp@gmail.com>
2024-06-05 13:07:35 -0500

    Metodo getAll student (no funciona por "Source must not be null")

--- riwiMultimedia/src/main/java/com/riwi/riwiMultimedia/api/controllers/StudentController.java
+++ riwiMultimedia/src/main/java/com/riwi/riwiMultimedia/api/controllers/StudentController.java
@@ -1,10 +1,13 @@
 package com.riwi.riwiMultimedia.api.controllers;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.riwi.riwiMultimedia.api.dto.request.StudentRequest;
@@ -28,5 +31,16 @@ public class StudentController {
     public ResponseEntity<StudentResponse> create(@Validated @RequestBody StudentRequest request){
         return ResponseEntity.ok(this.service.create(request));
     }
+
+    @Operation(summary = "list all students", description = "JSON format")
+    @GetMapping
+    public ResponseEntity<Page<StudentResponse>> getAll(
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "5") int size) {
+
+            return ResponseEntity.ok(this.service.getAll(page -1, size));
+
+        }
+    
 
 }
